1854dd100ccdb7cc545c722df64f352008e99c0d
Abraham <abrahamokaalo@gmail.com>
2023-11-14 19:58:26 +0000

    Classic and modern redux

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice-v1";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
+
+export default store;
